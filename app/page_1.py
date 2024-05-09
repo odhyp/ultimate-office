5348d6a63532c4bc62e35e5b9fd73a550a764174
odhyp <pradhana.odhy@gmail.com>
2024-05-09 21:23:16 +0700

    feat: add page_1.py for placeholder page

---
+++ app/page_1.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+# define three tabs
+tab1, tab2, tab3 = st.tabs(["Tab 1", "Tab 2", "Tab 3"])
+
+# tab 1
+with tab1:
+    st.title("Tab 1")
+    st.write("This is the content of Tab 1.")
+
+# tab 2
+with tab2:
+    st.title("Tab 2")
+    st.text("This is the content of Tab 2.")
+
+# tab 3
+with tab3:
+    st.title("Tab 3")
+    st.text("This is the content of Tab 3.")
