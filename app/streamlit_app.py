66eda01063686c81d404632ef8c5d9d40fb35b46
odhyp <pradhana.odhy@gmail.com>
2024-05-09 21:22:33 +0700

    feat: add streamlit_app.py as entrypoint

---
+++ app/streamlit_app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+from st_pages import show_pages_from_config
+
+st.set_page_config(
+    page_title="Welcome - Ultimate Office",
+    page_icon=":briefcase:"
+    )
+
+st.title("Welcome to Ultimate Office! :wave::nerd_face:")
+st.write("This is the home page!")
+
+show_pages_from_config()
