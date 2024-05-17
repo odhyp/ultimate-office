31ba4f6df9a59a8503038b3a0b360cca35cfe347
odhyp <pradhana.odhy@gmail.com>
2024-05-17 20:26:13 +0700

    test: add page test in page_1.py

--- app/page_1.py
+++ app/page_1.py
@@ -1,18 +1,35 @@
 import streamlit as st
 
+from src.xls_to_xlsx import convert_xls_to_xlsx
+
 st.set_page_config(
     page_title="File Converter - Ultimate Office",
     page_icon=":briefcase:",
     layout="wide"
 )
 
+
+def convert_xls():
+    pass
+    # st.title("XLS to XLSX")
+    # st.text("Convert .xls file to .xslx file")
+
+    # input_file = st.file_uploader(
+    #     "Upload your '.xls' file:", type=["xls"])
+
+    # if input_file is not None:
+    #     output_file = convert_xls_to_xlsx(input_file)
+
+    #     st.download_button("Download XLSX", output_file)
+
+
+
 # define three tabs
 tab1, tab2, tab3 = st.tabs(["Tab 1", "Tab 2", "Tab 3"])
 
 # tab 1
 with tab1:
-    st.title("Tab 1")
-    st.write("This is the content of Tab 1.")
+    convert_xls()
 
 # tab 2
 with tab2:
